77d8678ba46b4a0431455b3af7ea9902279af96f
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-11-10 02:07:41 +0600

    Final Push.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import ProductList from "./components/ProductList"
-import ProductDetails from "./components/ProductDetails"
-import AddProduct from "./components/AddProduct"
+import AddProduct from "./components/AddProduct";
+import ProductDetails from "./components/ProductDetails";
+import ProductList from "./components/ProductList";
 function App() {
   return (
     <div className="flex m-2">
       <AddProduct />
       <ProductList />
-      <ProductDetails id={5}/>
+      <ProductDetails id={5} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
